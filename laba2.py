e25cb51d387787e26d0b5b78ba7ec5193f2abaf6
TapkiNadenb <159562112+TapkiNadenb@users.noreply.github.com>
2024-03-02 11:05:41 +0400

    Update laba2.py

--- laba2.py
+++ laba2.py
@@ -1,24 +1,16 @@
+#ВАРИАНТ 16: Натуральные числа. Для каждого числа вывести используемые цифры (прописью) и их количество.
 import re
-
 slovarik = {
-    '0': 'ноль',
-    '1': 'раз',
-    '2': 'два',
-    '3': 'три',
-    '4': 'четыре',
-    '5': 'пятёрочка',
-    '6': 'шесть',
-    '7': 'семь',
-    '8': 'восемь',
-    '9': 'девять'
+    '0': 'ноль',    '1': 'раз',
+    '2': 'два',    '3': 'три',
+    '4': 'четыре',    '5': 'пятёрочка',
+    '6': 'шесть',    '7': 'семь',
+    '8': 'восемь',    '9': 'девять'
 }
-
 with open('myheard.txt', 'r') as f:
-    block = f.read(1024)
-    while block:
-        lexemes = re.findall(r'\b\d+\b', block)
-        for lexeme in lexemes:
-            nomer_str = ' '.join(slovarik[c] for c in lexeme)
-            schetchik_chisel = {c: lexeme.count(c) for c in lexeme}
-            print(f"Цифры прописью: {nomer_str}, Количество цифр: {len(lexeme)}, Используемые цифры и их количество: {schetchik_chisel}")
-        block = f.read()
+    block = f.read()
+    lexemes = re.findall(r'\b\d+\b', block)
+    for lexeme in lexemes:
+        nomer_str = ' '.join(slovarik[c] for c in lexeme)
+        schetchik_chisel = {c: lexeme.count(c) for c in lexeme}
+        print(f"Цифры прописью: {nomer_str}, Количество цифр: {len(lexeme)}, Используемые цифры и их количество: {schetchik_chisel}")
